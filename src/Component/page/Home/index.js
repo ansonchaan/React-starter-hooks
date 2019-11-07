0264ec8a37f08a4c82842fe3f73445f15d054b61
ansonchaan <ansonchan9523@gmail.com>
2019-11-07 18:06:20 +0800

    updates

--- src/Component/page/Home/index.js
+++ src/Component/page/Home/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { SmoothScroll } from '../../../globalFunc';
 import './home.scss';
 
 const Home = props => {
     const count = useSelector(state => state.count);
 
+    useEffect(()=>{ 
+        let smooth = new SmoothScroll('#scrollWrap',(s, y, h) => {});
+        smooth.on();
+        smooth.showScrollBar();
+
+        return () => {
+            smooth.hideScrollBar();
+            smooth.off();
+            smooth = null;
+        }
+    },[])
+
     return (
-        <div id="home">Home {count}
+        <div id="home">
+            <div id="scrollWrap">
+                Home {count}
+            </div>
         </div>
     )
 }
